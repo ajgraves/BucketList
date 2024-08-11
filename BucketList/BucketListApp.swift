86d8b9cc28fec648bd334a35490dcb84482d19d4
Aaron Graves <ajgraves@ajgraves.com>
2024-08-11 15:26:14 -0500

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Aaron Graves on 8/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
